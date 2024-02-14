bcde1a6244663baf3b9636804181ac7e80fd3f3d
Jared Sinasohn <81637028+JaredSinasohn4159@users.noreply.github.com>
2024-02-13 22:32:42 -0800

    Potentially working PID controller with doxygen files included.

--- src/controller.py
+++ src/controller.py
@@ -1,14 +1,24 @@
+"""!
+@file controller.py
+This file contains the class implementation for controlling a plant based on a sensor.
+The controller class implements a PID controller to control a sensor input based on a setpoint.
+This file also creates a test file to run an Ametek Pittman Motor to a set position based on
+an encoder input reading.
+
+@author Jared Sinasohn, Sydney Ulvick, Sean Nakashimo
+@date 22-Feb-2024
+"""
 import micropython
 import pyb
 import utime
 import encoder_reader.py
 import motor_driver.py
-class CLControl:
+class CLController:
     """! 
     This class implements a closed loop controller based on an input sensor.  This class uses previously created . 
     """
 
-    def __init__ (self, motor, sensor, kp, ki, kd, reference):
+    def __init__ (self, kp, ki, kd, setpoint):
         """! 
         Creates a motor driver by initializing GPIO
         pins and turning off the motor for safety. 
@@ -20,31 +30,84 @@ class CLControl:
         """
         self.sensor = sensor
         self.kp = kp
-        self.ki = kip
-        self.kd = kp
-        self.ref = reference
+        self.ki = ki
+        self.kd = kd
+        self.setpoint = setpoint
         # the effort the controller will send to the motor in percentage
         self.eff = 0
         # current value is the current reading of the sensor
         self.curr = self.sensor.read()
         # error is how far current value is from sensor
-        self.err = self.ref - self.curr
+        self.err = self.setpoint - self.curr
+        self.err_acc = 0
+        self.prev_err_list = []
+        self.deriv_amount = 10
+        self.update_time = 10
         print (f"Creating controller with kp = {kp}, ki = {ki}, and kd = {kd}")
-
-    def run(self):
+    
+    # why would this method accept the setpoint
+    def run(self, measured):
         """!
-        This method sets the duty cycle to be sent
-        to the motor based on the saturated effort calculated in this function.
+        This method calculates the the effort to apply to an actuator based
+        measured sensor value and a desired setpoint.  This method implements a
+        PID Controller approach
         """
-        self.curr = 
+        self.curr = measured
+        self.err = self.setpoint - self.curr
+        self.err_acc += self.err
+        self.eff = self.kd*self.err + self.ki*self.err_acc
+        self.prev_err_list.append(self.err)
+        if self.kd > 0 and self.prev_err_list >= self.deriv_amount
+            err_slope = (self.prev_err_list(self.deriv_amount-1)-self.prev_err_list(0))/(self.deriv_amount*self.update_time)
+            self.eff += self.kd*err_slope
+            self.prev_err_list.pop(0)            
+        return self.eff
 
+    def set_setpoint(self, setpoint)
+        self.setpoint = setpoint
+        
+    def set_kp(self, kp)
+        self.kp = kp
+    
+    def set_ki(self, ki)
+        self.ki = self.ki
+    
+    
+    
 if __name__ == "__main__":
-    en =  pyb.Pin(pyb.Pin.board.PA10, pyb.Pin.OUT_OD)
-    in1 = pyb.Pin(pyb.Pin.board.PB4, pyb.Pin.OUT_PP)
-    in2 = pyb.Pin(pyb.Pin.board.PB5, pyb.Pin.OUT_PP)
-    tim3 = pyb.Timer(3, freq=20000)
-    motor = MotorDriver(en,in1,in2,tim3)
-    try:
-        motor.set_duty_cycle(0)
-    except:
-        motor.set_duty_cycle(0)
+    sleep_time = 10
+    # create pin to power motor
+    en_pin =  pyb.Pin(pyb.Pin.board.PA10, mode = pyb.Pin.OPEN_DRAIN, pull = pyb.Pin.PULL_UP, value=1)
+    
+    # create first pwm pin
+    in1pin = pyb.Pin(pyb.Pin.board.PB4, pyb.Pin.OUT_PP)
+    
+    # create first pwm pin
+    in2pin = pyb.Pin(pyb.Pin.board.PB5, pyb.Pin.OUT_PP)
+    
+    # create timer for pwm
+    timer = pyb.Timer(3, freq=5000) #setting frequency for motor
+    
+    # create motor object
+    motor = MotorDriver(en_pin,in1pin,in2pin,timer,ch1,ch2) #call to the motor class you just made!
+
+    # create the pin object to read encoder channel A
+    pin1 = pyb.Pin(pyb.Pin.board.PC6, pyb.Pin.IN)
+    
+    # create the pin object to read encoder channel B
+    pin2 = pyb.Pin(pyb.Pin.board.PC7, pyb.Pin.IN)
+    
+    # create the timer object.  For C6 and C7 use timer 8,
+    # set the prescaler to zero and the period to the max 16bit number
+    timer = pyb.Timer(8, prescaler = 0, period = 65535)
+    
+    # create the encoder object
+    encoder = Encoder(pin1, pin2, timer)
+    
+    # create controller object
+    con = CLController(.5, 0, 0, 10000)
+    while True:
+        measured = encoder.read()
+        eff = con.run(measured)
+        motor.set_duty_cycle(eff)
+        utime.sleep_ms(sleep_time)
--- src/motor_driver.py
+++ src/motor_driver.py
@@ -1,38 +1,46 @@
 """!
 @file motor_driver.py
-This file contains the class implementation for powering the ametek pittman motors via the ME 405 microcontroller.
-This file also contains testing code to test if motor can be spun, and we can set the duty cycle to various levels by stopping
-and restarting the code.
+This file contains the class implementation for powering an ametek pittman motor.
+This file also contains testing code to test if the motor runs, and if it can handle
+bad pwm levels.
 
 @author Jared Sinasohn, Sydney Ulvick, Sean Nakashimo
 @date 15-Feb-2024
 """
-import micropython
 import pyb
 import utime
+import micropython
+
 class MotorDriver:
     """! 
-    This class implements a motor driver for an ME405 kit. This class can set
-    the motor duty cycle from percentages from -100 to 100
+    This class implements a motor driver for an ME405 kit. 
     """
 
     def __init__ (self, en_pin, in1pin, in2pin, timer):
         """! 
         Creates a motor driver by initializing GPIO
-        pins and turning off the motor for safety. 
-        @param en_pin (There will be several parameters)
+        pins and turning off the motor for safety.
+        @param en_pin: enabling pin for the motor
+        @param in1pin: where the PWM is being sent for 1
+        @param in2pin: where the PWM is being sent for 2
+        @param timer: timer that the motor uses for the PWM
+        @param ch1: where the timer is being channeled to send to pin1
+        @param ch2: where the timer is being channeled to send to pin2
         """
-        self.en_pin = en_pin
+        print ("Creating a motor driver")
+        # defining parameters needed to characterize motor
+        self.en_pin = en_pin 
         self.in1pin = in1pin
         self.in2pin = in2pin
         self.timer = timer
-        self.en_pin.high()
+        self.en_pin.low() #disable the motor (for safety)
         self.ch1 = self.timer.channel(1, pyb.Timer.PWM, pin=self.in1pin)
         self.ch2 = self.timer.channel(2, pyb.Timer.PWM, pin=self.in2pin)
         self.ch1.pulse_width_percent(0)
         self.ch2.pulse_width_percent(0)
         print ("Creating a motor driver")
-
+    
+        
     def set_duty_cycle (self, level):
         """!
         This method sets the duty cycle to be sent
@@ -42,32 +50,50 @@ class MotorDriver:
         @param level A signed integer holding the duty
                cycle of the voltage sent to the motor 
         """
-        print (f"Setting duty cycle to {level}")
-        if level < 0:
-            self.en_pin.high()
-            self.ch1.pulse_width_percent(0)
-            if level >= -100:
-                self.ch2.pulse_width_percent(abs(level))
+        #setting the duty cycle
+        try:
+            level = float(level)
+            if level < 0: #for negative in range
+                print (f"Setting duty cycle to {level}")
+                print('negative in range') #debugging
+                self.en_pin.high() #enable the motor
+                self.ch2.pulse_width_percent(0) #set in2 to zero
+                # if the level is greater than -100, nothing happens
+                # if it is less than -100, set the PWM to 100
+                # this saturates the level to -100 regardless of how big the level is
+                if level >= -100:
+                    self.ch1.pulse_width_percent(abs(level))
+                else:
+                    self.ch1.pulse_width_percent(100)
+            elif level > 0: #for positive in range
+                print (f"Setting duty cycle to {level}")
+                print('positive in range') #debugging
+                self.en_pin.high() #enable the motor
+                self.ch1.pulse_width_percent(0) #set in1 to zero
+                # if the level is less than 100, nothing happens
+                # if it is greater than 100, set the PWM to 100
+                # this saturates the level to 100 regardless of how big the level is
+                if level <= 100:
+                    self.ch2.pulse_width_percent(level)
+                else:
+                    self.ch2.pulse_width_percent(100)  
             else:
-                self.ch2.pulse_width_percent(100)
-        elif level > 0:
-            self.en_pin.high()
-            self.ch2.pulse_width_percent(0)
-            if level <= 100:
-                self.ch1.pulse_width_percent(level)
-            else:
-                self.ch1.pulse_width_percent(100)                
-        else:
-            self.ch1.pulse_width_percent(0)
-            self.ch2.pulse_width_percent(0)
-
+                print (f"Setting duty cycle to {level}")
+                self.ch1.pulse_width_percent(0)
+                self.ch2.pulse_width_percent(0)
+        except ValueError:
+            print('invalid level value')
+        
 if __name__ == "__main__":
-    en =  pyb.Pin(pyb.Pin.board.PA10, pyb.Pin.OUT_OD)
-    in1 = pyb.Pin(pyb.Pin.board.PB4, pyb.Pin.OUT_PP)
-    in2 = pyb.Pin(pyb.Pin.board.PB5, pyb.Pin.OUT_PP)
-    tim3 = pyb.Timer(3, freq=20000)
-    motor = MotorDriver(en,in1,in2,tim3)
-    try:
-        motor.set_duty_cycle(0)
-    except:
-        motor.set_duty_cycle(0)
+    # power the motor for five seconds 
+    en_pin =  pyb.Pin(pyb.Pin.board.PA10, mode = pyb.Pin.OPEN_DRAIN, pull = pyb.Pin.PULL_UP, value=1)
+    in1pin = pyb.Pin(pyb.Pin.board.PB4, pyb.Pin.OUT_PP)
+    in2pin = pyb.Pin(pyb.Pin.board.PB5, pyb.Pin.OUT_PP)
+    timer = pyb.Timer(3, freq=5000) #setting frequency for motor 
+    motor = MotorDriver(en_pin,in1pin,in2pin,timer,ch1,ch2) #call to the motor class you just made!
+    motor.set_duty_cycle(50) #set duty cycle, in range -100 to 100 (not including 0)
+    utime.sleep(5)
+    motor.set_duty_cycle(0)
+    
+
+
